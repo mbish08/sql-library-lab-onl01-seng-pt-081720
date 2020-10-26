9b6c3db46284e68a23c22d7e21f6cc4cd3091190
Mikel Bishop <mdrew8@gmail.com>
2020-10-26 02:49:41 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,11 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, series"
+  "SELECT authors.name, subgenres.name FROM series
+  INNER JOIN authors
+    ON series.author_id = authors.id
+  INNER JOIN subgenres
+    ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
@@ -29,3 +33,4 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "Write your SQL query here"
 end
+
