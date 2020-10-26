2383c95de6a8d065c25559fb21be97e063a7e41d
Mikel Bishop <mdrew8@gmail.com>
2020-10-26 02:08:06 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,4 +2,19 @@ INSERT INTO series (title) VALUES ("Black Jewels");
 INSERT INTO series (title) VALUES ("Noble Dead");
 
 INSERT INTO books (title, year, series_id) 
-VALUES ();
+VALUES ("Daughter of the Blood", 1998, 1);
+
+INSERT INTO books (title, year, series_id) 
+VALUES ("Heir to the Shadows", 1999, 1);
+
+INSERT INTO books (title, year, series_id) 
+VALUES ("Queen of the Darkness", 2000, 1);
+
+INSERT INTO books (title, year, series_id) 
+VALUES ("Dhampir", 2003, 2);
+
+INSERT INTO books (title, year, series_id) 
+VALUES ("Thief of Lives", 2004, 2);
+
+INSERT INTO books (title, year, series_id) 
+VALUES ("Sister of the Dead", 2005, 2);
