3af7d33e63f3db9b0ede7eb5bb43e7463cde7ad7
Mikel Bishop <mdrew8@gmail.com>
2020-10-26 02:42:52 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,11 +16,11 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  ""
+  "SELECT name"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT species, COUNT(species) FROM characters"
 end
 
 def select_character_names_and_number_of_books_they_are_in
